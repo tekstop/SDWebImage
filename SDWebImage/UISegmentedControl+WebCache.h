0edaa77f58546927e99ffa858b70dffb154af7b3
Chirag Gupta <thechimerical@gmail.com>
2012-12-28 15:28:11 -0500

    Added UISegmentedControl+WebCache Category

---
+++ SDWebImage/UISegmentedControl+WebCache.h
@@ -0,0 +1,101 @@
+//
+//  UISegmentedControl+WebCache.h
+//  SDWebImage
+//
+//  Created by Chirag Gupta on 12/24/12.
+//  Copyright (c) 2012 Dailymotion. All rights reserved.
+//
+
+#import "SDWebImageCompat.h"
+#import "SDWebImageManager.h"
+
+
+@interface UISegmentedControl (WebCache)
+
+/**
+ * Set the imageView `image` with an `url`.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment;
+
+/**
+ * Set the imageView `image` with an `url` and a placeholder.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ * @param placeholder The image to be set initially, until the image request finishes.
+ * @see setImageWithURL:placeholderImage:options:
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder;
+
+/**
+ * Set the imageView `image` with an `url`, placeholder and custom options.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ * @param placeholder The image to be set initially, until the image request finishes.
+ * @param options The options to use when downloading the image. @see SDWebImageOptions for the possible values.
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options;
+
+/**
+ * Set the imageView `image` with an `url`.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ * @param completedBlock A block called when operation has been completed. This block as no return value
+ *                       and takes the requested UIImage as first parameter. In case of error the image parameter
+ *                       is nil and the second parameter may contain an NSError. The third parameter is a Boolean
+ *                       indicating if the image was retrived from the local cache of from the network.
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment completed:(SDWebImageCompletedBlock)completedBlock;
+
+/**
+ * Set the imageView `image` with an `url`, placeholder.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ * @param placeholder The image to be set initially, until the image request finishes.
+ * @param completedBlock A block called when operation has been completed. This block as no return value
+ *                       and takes the requested UIImage as first parameter. In case of error the image parameter
+ *                       is nil and the second parameter may contain an NSError. The third parameter is a Boolean
+ *                       indicating if the image was retrived from the local cache of from the network.
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder completed:(SDWebImageCompletedBlock)completedBlock;
+
+/**
+ * Set the imageView `image` with an `url`, placeholder and custom options.
+ *
+ * The downloand is asynchronous and cached.
+ *
+ * @param url The url for the image.
+ * @param segmentIndex
+ * @param placeholder The image to be set initially, until the image request finishes.
+ * @param options The options to use when downloading the image. @see SDWebImageOptions for the possible values.
+ * @param completedBlock A block called when operation has been completed. This block as no return value
+ *                       and takes the requested UIImage as first parameter. In case of error the image parameter
+ *                       is nil and the second parameter may contain an NSError. The third parameter is a Boolean
+ *                       indicating if the image was retrived from the local cache of from the network.
+ */
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options completed:(SDWebImageCompletedBlock)completedBlock;
+
+/**
+ * Cancel the current download
+ */
+- (void)cancelCurrentImageLoad;
+
+
+
+@end
---
+++ SDWebImage/UISegmentedControl+WebCache.m
@@ -0,0 +1,76 @@
+//
+//  UISegmentedControl+WebCache.m
+//  SDWebImage
+//
+//  Created by Chirag Gupta on 12/24/12.
+//  Copyright (c) 2012 Dailymotion. All rights reserved.
+//
+
+#import "UISegmentedControl+WebCache.h"
+#import "objc/runtime.h"
+
+static char operationKey;
+
+@implementation UISegmentedControl (WebCache)
+
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment
+{
+    [self setImageWithURL:url forSegmentAtIndex:segment placeholderImage:nil options:0 completed:nil];
+}
+
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder
+{
+    [self setImageWithURL:url forSegmentAtIndex:segment placeholderImage:placeholder options:0 completed:nil];
+}
+
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options
+{
+    [self setImageWithURL:url forSegmentAtIndex:segment placeholderImage:placeholder options:options completed:nil];
+}
+
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment completed:(SDWebImageCompletedBlock)completedBlock
+{
+    [self setImageWithURL:url forSegmentAtIndex:segment placeholderImage:nil options:0 completed:completedBlock];
+}
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder completed:(SDWebImageCompletedBlock)completedBlock
+{
+    [self setImageWithURL:url forSegmentAtIndex:segment placeholderImage:placeholder options:0 completed:completedBlock];
+}
+
+- (void)setImageWithURL:(NSURL *)url forSegmentAtIndex:(NSUInteger)segment placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options completed:(SDWebImageCompletedBlock)completedBlock
+{
+    //[self cancelCurrentImageLoad];
+    
+    [self setImage:placeholder forSegmentAtIndex:segment];
+    
+    if (url)
+    {
+        __weak UISegmentedControl *wself = self;
+        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL:url options:options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished)
+                                             {
+                                                 __strong UISegmentedControl *sself = wself;
+                                                 if (!sself) return;
+                                                 if (image)
+                                                 {
+                                                     [sself setImage:image forSegmentAtIndex:segment];
+                                                 }
+                                                 if (completedBlock && finished)
+                                                 {
+                                                     completedBlock(image, error, cacheType);
+                                                 }
+                                             }];
+        objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+}
+
+- (void)cancelCurrentImageLoad
+{
+    // Cancel in progress downloader from queue
+    id<SDWebImageOperation> operation = objc_getAssociatedObject(self, &operationKey);
+    if (operation)
+    {
+        [operation cancel];
+        objc_setAssociatedObject(self, &operationKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+}
+@end
